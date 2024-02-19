89402251d3572369b56a7d5686f0feee5d86c26d
longnt <longnt.225@gmail.com>
2024-02-19 17:03:25 +0700

    upload project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useColorScheme } from '@mui/material'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Container from '@mui/material/Container'
 
-function App() {
-  const [count, setCount] = useState(0)
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FormControl size='small'>
+      <InputLabel id='mode-theme-label'>Mode</InputLabel>
+      <Select
+        labelId='mode-theme-label'
+        id='mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>Light</MenuItem>
+        <MenuItem value='dark'>Dark</MenuItem>
+        <MenuItem value='system'>System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+function App() {
+  return (
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{ height: '100vh', backgroundColor: 'primary.main' }}
+    >
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: '48px',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <ModeToggle />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: '58px',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          height: 'calc(100vh - 58px - 48px)',
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Content
+      </Box>
+    </Container>
   )
 }
 
